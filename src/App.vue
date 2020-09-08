08c9fd123b6be6a9dfb215bde85d96b00ebe1d3e
jh46666 <1039460820@qq.com>
2020-09-09 00:31:54 +0800

    electron 的功能测试

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,228 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="titleBar">
+      <div class="title">
+        <div class="logo">
+          <img src="@/assets/logo.png" alt="" />
+        </div>
+        <div class="txt">窗口标题</div>
+      </div>
+      <div class="windowTool">
+        <div @click="minisize">
+          <i class="iconfont iconminisize"></i>
+        </div>
+        <div v-if="isMaxSize" @click="restore">
+          <i class="iconfont iconrestore"></i>
+        </div>
+        <div v-else @click="maxsize">
+          <i class="iconfont iconmaxsize"></i>
+        </div>
+        <div @click="close" class="close">
+          <i class="iconfont iconclose"></i>
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <div class="content">
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+      <div @click="openModel" style="font-caption">点击modal</div>
+      <router-view />
+    </div>
   </div>
 </template>
 
+<script>
+const { remote, shell } = require('electron');
+// const clipboard = require('clipboard-files');
+
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      isMaxSize: false,
+    };
+  },
+  mounted() {
+    const win = remote.getCurrentWindow();
+    win.on('maximize', () => {
+      this.isMaxSize = true;
+      this.setState();
+    });
+    win.on('unmaximize', () => {
+      this.isMaxSize = false;
+      this.setState();
+    });
+    win.on('move', this.debounce(() => {
+      this.setState();
+    }));
+    win.on('resize', this.debounce(() => {
+      this.setState();
+    }));
+    this.isMaxSize = win.isMaximized;
+    this.getState();
+    shell.openExternal('https://www.baidu.com');
+  },
+  methods: {
+    close() {
+      remote.getCurrentWindow().close();
+    },
+    debounce(fn) {
+      let timeout = null;
+      return function () {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+          // eslint-disable-next-line prefer-rest-params
+          fn.apply(this, arguments);
+        }, 300);
+      };
+    },
+    minisize() {
+      remote.getCurrentWindow().minimize();
+    },
+    restore() {
+      remote.getCurrentWindow().restore();
+    },
+    maxsize() {
+      remote.getCurrentWindow().maxmize();
+    },
+    getState() {
+      const win = remote.getCurrentWindow();
+      let winState = localStorage.getItem('winstate');
+
+      if (winState) {
+        winState = JSON.parse(winState);
+        if (winState.isMaxSize) win.maximize();
+        else win.setBounds(winState.rect);
+      }
+    },
+    setState() {
+      const win = remote.getCurrentWindow();
+      const rect = win.getBounds();
+      const isMaxSize = win.isMaximized();
+      const obj = { rect, isMaxSize };
+      localStorage.setItem('winState', JSON.stringify(obj));
+    },
+    async openModel() {
+      /*    this.win = new remote.BrowserWindow({
+        parent: remote.getCurrentWindow(),
+        modal: false,
+        webPreferences: {
+          nodeIntegration: true,
+        },
+      }); */
+
+      // const fileNames = clipboard.readFiles();
+      // console.log('fileNames: ', fileNames);
+
+      const { dialog, app, session } = remote;
+      console.log('dialog, app: ', dialog, app, session);
+      const filePath = await dialog.showOpenDialog({
+        title: '我需要打开一个文件',
+        buttonLabel: '按此打开文件',
+        defaultPath: app.getPath('pictures'),
+        properties: 'multiSelections',
+        filters: [
+          { name: '图片', extensions: ['jpg', 'png', 'gif'] },
+          { name: '视频', extensions: ['mkv', 'avi', 'mp4'] },
+        ],
+      });
+      console.log('file', filePath);
+
+      /*     const getCookie = async function (name) {
+        const cookie = session.defaultSession.cookies.get({ name });
+        if (cookie.length > 0) {
+          return cookie[0].value;
+        }
+        return 0;
+      };
+
+      const setCookie = async function (cookie) {
+        await session.defaultSession.cookies.set(cookie);
+        await session.defaultSession.clearStorageData({storages: 'cookie,localstorage'})
+      }; */
+
+      // app.clearRecentDocuments('xxx');
+      // app.clearRecentDocuments();
+    },
+  },
+};
+</script>
+
 <style lang="scss">
+body,
+html {
+  margin: 0px;
+  padding: 0px;
+  overflow: hidden;
+  height: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  height: 100%;
+  overflow: hidden;
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+  border: 1px solid #f5222d;
+  display: flex;
+  flex-direction: column;
   text-align: center;
-  color: #2c3e50;
 }
+</style>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<style lang="scss" >
+@import url(https://at.alicdn.com/t/font_1378132_s4e44adve5.css);
+.titleBar {
+  height: 38px;
+  line-height: 36px;
+  background: #fff1f0;
+  display: flex;
+  border-bottom: 1px solid #f5222d;
+  .title {
+    flex: 1;
+    display: flex;
+    -webkit-app-region: drag;
+    .logo {
+      padding-left: 8px;
+      padding-right: 6px;
+      img {
+        width: 20px;
+        height: 20px;
+        margin-top: 7px;
+      }
+    }
+    .txt {
+      text-align: left;
+      flex: 1;
     }
   }
+  .windowTool {
+    div {
+      color: #888;
+      height: 100%;
+      width: 38px;
+      display: inline-block;
+      cursor: pointer;
+      i {
+        font-size: 12px;
+      }
+      &:hover {
+        background: #ff4d4f;
+      }
+    }
+    .close:hover {
+      color: #fff;
+      background: #ff4d4f;
+    }
+  }
+}
+.content {
+  flex: 1;
+  overflow-y: auto;
+  overflow-x: auto;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -33,10 +33,30 @@
 </template>
 
 <script>
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String,
+  },
+  mounted() {
+    const adapter = new FileSync('db.json');
+    const db = low(adapter);
+
+    // Set some defaults
+    db.defaults({ posts: [], user: {} })
+      .write();
+
+    // Add a post
+    db.get('posts')
+      .push({ id: 1, title: 'lowdb is awesome' })
+      .write();
+
+    // Set a user using Lodash shorthand syntax
+    db.set('user.name', 'typicode')
+      .write();
   },
 };
 </script>
